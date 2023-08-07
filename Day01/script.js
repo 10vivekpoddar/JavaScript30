290f1f2b031d2811eda229e5687bb4eebe426de9
Vivek Poddar <63947553+10vivekpoddar@users.noreply.github.com>
2023-08-07 12:19:10 +0530

    doc - Added comments in js file

--- Day01/script.js
+++ Day01/script.js
@@ -1,3 +1,10 @@
+/*
+  Function for playing the audio
+  @param (audio, key)
+
+  * If the audio or key is null return
+  * We will add the playing class to the selected div
+*/
 function audioPlay(audio, key) {
   if (!audio && !key) return;
   key.classList.add("playing");
@@ -5,6 +12,14 @@ function audioPlay(audio, key) {
   audio.play();
 }
 
+/*
+  Playing the audio by clicking
+    * We will add a event listener on all the key-divs
+    * If any key is clicked, we will fetch the data-key attribute
+    * Search the audio which has the similar data-key
+    * play the music
+
+*/
 function playOnClick(key) {
   key.classList.add("key:hover");
   key.addEventListener("click", function () {
@@ -14,6 +29,12 @@ function playOnClick(key) {
   });
 }
 
+/*
+  Playing the audio by pressing the key:-
+    * we will fetch the press key through keydown by adding event listener to the window.
+    * Search the audio and div, which has data-key equal to the presses keycode
+    * play the music
+*/
 window.addEventListener("keydown", function (e) {
   const audio = this.document.querySelector(`audio[data-key='${e.keyCode}']`);
   const key = this.document.querySelector(`.key[data-key='${e.keyCode}']`);
@@ -23,7 +44,10 @@ window.addEventListener("keydown", function (e) {
 const keys = document.querySelectorAll(".key");
 keys.forEach(playOnClick);
 
-document.addEventListener("transitionend", function (e) {
+/*
+  Removing the transition effect
+*/
+window.addEventListener("transitionend", function (e) {
   keys.forEach((key) => key.classList.remove("playing"));
   audio.pause();
 });

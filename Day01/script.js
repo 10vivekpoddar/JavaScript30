321d07d0377908fa741d3224094796e8578c3bdc
Vivek Poddar <63947553+10vivekpoddar@users.noreply.github.com>
2023-08-07 11:28:27 +0530

    Completed Day1 challenge
    Added a new feature from my end:
    
    By clicking on the shown key, user can play the audio as well.

---
+++ Day01/script.js
@@ -0,0 +1,32 @@
+function audioPlay(audio, key) {
+  if (!audio && !key) return;
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+  setTimeout(() => {
+    audio.pause();
+  }, 2500);
+}
+
+function playOnClick(key) {
+  key.classList.add("key:hover");
+  key.addEventListener("click", function () {
+    const pressKey = key.getAttribute("data-key");
+    const audio = document.querySelector(`audio[data-key='${pressKey}']`);
+    audioPlay(audio, key);
+  });
+}
+
+window.addEventListener("keydown", function (e) {
+  const audio = this.document.querySelector(`audio[data-key='${e.keyCode}']`);
+  const key = this.document.querySelector(`.key[data-key='${e.keyCode}']`);
+  audioPlay(audio, key);
+});
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(playOnClick);
+
+document.addEventListener("transitionend", function (e) {
+  keys.forEach((key) => key.classList.remove("playing"));
+  audio.pause();
+});
